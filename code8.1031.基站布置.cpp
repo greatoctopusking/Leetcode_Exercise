e721dc02f71fd88ea91d430b8ce682a6a9e9ed5c
greatoctopusking <great_octopus_king@outlook.com>
2026-05-01 09:16:28 +0800

    Create code8.1031.基站布置.cpp

---
+++ code8.1031.基站布置.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+using namespace std;
+int m;
+int n, d;
+
+struct Island {
+    int x; int y;
+    double l, r;
+    int idx;
+
+    Island(int xx, int yy, int id) :x(xx), y(yy), idx(id) {
+        double len = pow(d * d - y * y, 0.5);
+        l = x - len; r = x + len;
+    }
+};
+
+struct left_cmp {
+    bool operator()(const Island& a, const Island& b)const {
+        return a.l > b.l;
+    }
+};
+
+struct right_cmp {
+    bool operator()(const Island& a, const Island& b)const {
+        return a.r > b.r;
+    }
+};
+
+int main() {
+    scanf("%d", &m);
+    while (m--) {
+        scanf("%d%d", &n, &d);
+
+        priority_queue<Island, vector<Island>, left_cmp> left_min_heap;
+        priority_queue<Island, vector<Island>, right_cmp> right_min_heap;
+        vector<bool> covered(n, false);
+
+        int xx, yy;
+        for (int i = 0; i < n; i++) {
+            scanf("%d%d", &xx, &yy);
+            left_min_heap.push(Island(xx, yy, i));
+            right_min_heap.push(Island(xx, yy, i));
+        }
+
+        int res = 0;
+        while (!right_min_heap.empty()) {
+            if (covered[right_min_heap.top().idx]) { right_min_heap.pop(); continue; }
+            else {
+                res++;
+                covered[right_min_heap.top().idx] = true;
+                double right = right_min_heap.top().r;
+                right_min_heap.pop();
+                while (!left_min_heap.empty() && left_min_heap.top().l - right <= 1e-10) {
+                    covered[left_min_heap.top().idx] = true;
+                    left_min_heap.pop();
+                }
+            }
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
